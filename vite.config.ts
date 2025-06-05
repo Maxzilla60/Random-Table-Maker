6bace170a4a8b0495d9fc9e45cf7a77dc9badc18
Maxzilla60 <maxpower98@hotmail.com>
2025-06-05 17:22:20 +0200

    ➕ Install vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [svelte()],
+	resolve: process.env.VITEST ? { conditions: ['browser'] } : undefined,
 });
